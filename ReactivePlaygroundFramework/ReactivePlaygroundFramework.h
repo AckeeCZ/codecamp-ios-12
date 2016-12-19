690d047f3e8281094d00e4a6a2908e36a7551496
Petr Sima <pt@petrs-macbook-pro.local>
2016-12-19 22:35:54 +0100

    Initial Commit

---
+++ ReactivePlaygroundFramework/ReactivePlaygroundFramework.h
@@ -0,0 +1,19 @@
+//
+//  ReactivePlaygroundFramework.h
+//  ReactivePlaygroundFramework
+//
+//  Created by Petr Sima on 19/12/2016.
+//  Copyright © 2016 Ackee, s.r.o. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReactivePlaygroundFramework.
+FOUNDATION_EXPORT double ReactivePlaygroundFrameworkVersionNumber;
+
+//! Project version string for ReactivePlaygroundFramework.
+FOUNDATION_EXPORT const unsigned char ReactivePlaygroundFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactivePlaygroundFramework/PublicHeader.h>
+
+
